88ddc0a0d139e2c3770d6ba40017aefa519a586e
Jackson <635725626@qq.com>
2021-05-30 14:28:36 +0800

    translate

--- utils/mergeData.py
+++ utils/mergeData.py
@@ -1,18 +1,17 @@
-nums = 956
+nums = 50
 
-data_file_path = '../data/LCQMC/translation/en_split/train/'
-written_file = '../data/LCQMC/translation/train_en.txt'
+data_file_path = '../data/LCQMC/translation/en_split/test/'
+written_file = '../data/LCQMC/translation/test_en.txt'
 
 all_datas = []
 wrong_data = []
 for i in range(0, nums):
-    with open(data_file_path + 'train_split' + str(i + 1) + '.txt', 'r', encoding='utf-8') as reader:
+    with open(data_file_path + 'test_split' + str(i + 1) + '.txt', 'r', encoding='utf-8') as reader:
         lines = reader.readlines()
         for j in range(len(lines)):
             line = lines[j]
             all_datas.append(line)
-print(len(all_datas))
-
+#
 with open(written_file, 'w', encoding='utf-8') as writer:
     for a_data in all_datas:
         writer.write(a_data.strip()+'\n')
