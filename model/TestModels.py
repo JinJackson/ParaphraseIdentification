90e5cc879fe990478e84d49e355e627a92bd73a2
JacksonJin <635725626@qq.com>
2021-06-11 08:42:01 +0800

    t

--- model/TestModels.py
+++ model/TestModels.py
@@ -1,23 +1,26 @@
-from transformers import BertModel, BertTokenizer
-from model.VAEMatchModel import VaeBertMatchModel
-import torch
-import torch.nn as nn
+# from transformers import BertModel, BertTokenizer
+# from model.VAEMatchModel import VaeBertMatchModel
+# import torch
+# import torch.nn as nn
+#
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+#
+# Text = 'I feel good today.'
+#
+# inputs = tokenizer(Text, max_length=128, padding='max_length', return_tensors='pt')
+# labels = torch.tensor([[1]]).float()
+# # inputs.to(device)
+# model = VaeBertMatchModel.from_pretrained('bert-base-uncased')
+# # model.to(device)
+#
+# loss, logits = model(**inputs, labels=labels)
+#
+# # loss.backward()
+#
+#
+# print(loss, logits)
+#
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-Text = 'I feel good today.'
-
-inputs = tokenizer(Text, max_length=128, padding='max_length', return_tensors='pt')
-labels = torch.tensor([[1]]).float()
-# inputs.to(device)
-model = VaeBertMatchModel.from_pretrained('bert-base-uncased')
-# model.to(device)
-
-loss, logits = model(**inputs, labels=labels)
-
-# loss.backward()
-
-
-print(loss, logits)
 
--- utils/QtypeMarking.py
+++ utils/QtypeMarking.py
@@ -3,6 +3,7 @@ translate_file = '../data/LCQMC/translation/train_en.txt'
 
 from tqdm import tqdm
 
+# 读取原始数据
 def readDataFromFile(data_file):
     datas = []
     with open(data_file, 'r', encoding='utf-8') as reader:
@@ -13,6 +14,7 @@ def readDataFromFile(data_file):
     return datas
 
 
+# 读取原始数据的翻译数据
 def readTranslateData(data_file):
     datas = []
     with open(data_file, 'r', encoding='utf-8') as reader:
@@ -37,34 +39,65 @@ def readTranslateData(data_file):
     return datas
 
 
-# def markQtype(sentence)
-all_datas = readDataFromFile(data_file)
-all_trans = readTranslateData(translate_file)
+# 标记数据的Qtype
+def tagging_data(origin_datas, trans_datas):
+    assert len(origin_datas) == len(trans_datas)
+    all_taggings = []
+    for origin_data, trans_data in tqdm(zip(origin_datas, trans_datas), total=len(origin_datas)):
+        # import pdb
+        # pdb.set_trace()
+        origin_query1, origin_query2, label = origin_data
+        if len(trans_data) == 4:
+            all_taggings.append([origin_query1, origin_query2, label, ['UNK'], ['UNK']])
+            continue
+        origin_query1, origin_query2, label = origin_data
+        try:
+            trans_query1, trans_query2, label = trans_data
+            qtype_words = ['what', 'who', 'how', 'which', 'when', 'where', 'why']
+            q_type1 = []
+            q_type2 = []
+            trans_query1 = trans_query1.lower()
+            trans_query2 = trans_query2.lower()
+            for qtype_word in qtype_words:
+                if qtype_word in trans_query1:
+                    q_type1.append(qtype_word)
+                if qtype_word in trans_query2:
+                    q_type2.append(qtype_word)
+            else:
+                if not q_type1:
+                    q_type1 = ['others']
+                if not q_type2:
+                    q_type2 = ['others']
+            all_taggings.append([origin_query1, origin_query2, label, q_type1, q_type2])
+        except:
+            all_taggings.append([origin_query1, origin_query2, label, ['UNK'], ['UNK']])
+    return all_taggings
 
-print(len(all_datas), len((all_trans)))
 
-# tagging data
-all_taggings = []
-for origin_data, trans_data in tqdm(zip(all_datas, all_trans), total=len(all_datas)):
-    origin_query1, origin_query2, label = origin_data
-    if len(trans_data) == 4:
-        all_taggings.append([origin_query1, origin_query2, label, ['UNK'], ['UNK']])
-    origin_query1, origin_query2, label = origin_data
-    try:
-        trans_query1, trans_query2, label = trans_data
-        qtype_words = ['what', 'who', 'how', 'which', 'when', 'where', 'why', 'others']
-        q_type1 = []
-        q_type2 = []
-        for qtype_word in qtype_words:
-            if qtype_word in trans_query1:
-                q_type1.append(qtype_word)
-            if q_type2 in trans_query2:
-                q_type2.append(qtype_word)
-        else:
-            if not q_type1:
-                q_type1 = ['others']
-            if not q_type2:
-                q_type2 = ['others']
-        all_taggings.append([trans_query1, trans_query2, label, q_type1, q_type2])
-    except:
-        all_taggings.append([origin_query1, origin_query2, label, ['UNK'], ['UNK']])
+# # 写入数据
+def written_file(labeled_data, written_file):
+    with open(written_file, 'w', encoding='utf-8') as writer:
+        for a_data in labeled_data:
+            query1, query2, label, qmark1, qmark2 = a_data
+            str_qmark1 = ''
+            for q_word in qmark1:
+                str_qmark1 += (q_word + ' ')
+            str_qmark2 = ''
+            for q_word in qmark2:
+                str_qmark2 += (q_word + ' ')
+            line = query1 + query2
+
+
+
+
+if __name__ == '__main__':
+
+    origin_datas = readDataFromFile(data_file)
+    trans_datas = readTranslateData(translate_file)
+    print(len(origin_datas), len(trans_datas))
+    all_tagging_datas = tagging_data(origin_datas, trans_datas)
+    print(len(all_tagging_datas))
+    for i in range(5):
+        print(all_tagging_datas[i])
+    # for data in all_taggings:
+    #     print(data)
