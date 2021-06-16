f770ffcce5be603b1fb5656d4511d9f021722a0e
JacksonJin <635725626@qq.com>
2021-06-16 12:59:36 +0800

    model

--- model/TestModels.py
+++ model/TestModels.py
@@ -1,7 +1,24 @@
 from model.MatchModel import ErnieMatchModel
 from transformers import AutoTokenizer
+import torch
 
 model = ErnieMatchModel.from_pretrained("nghuyong/ernie-1.0")
 tokenizer = AutoTokenizer.from_pretrained("nghuyong/ernie-1.0")
 
-# print(model)
+#print(model)
+
+text = '今天的天气真不错'
+label = torch.tensor([[1]])
+
+inputs = tokenizer.encode_plus(text=text, return_tensors='pt')
+
+input_ids = inputs['input_ids']
+token_type_ids = inputs['token_type_ids']
+attention_mask = inputs['attention_mask']
+
+outputs = model(input_ids=input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask, labels=label)
+
+loss, logits = outputs
+
+print(loss)
+print(logits)
