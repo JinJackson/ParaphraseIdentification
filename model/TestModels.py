14e2f357254d34678614e9c96477bf4d8bd378ce
JacksonJin <635725626@qq.com>
2021-06-22 15:32:24 +0800

    fix

--- model/TestModels.py
+++ model/TestModels.py
@@ -6,47 +6,47 @@ from torch.nn import CrossEntropyLoss
 
 from attention import attention
 
-# class TestModel(BertPreTrainedModel):
-#     def __init__(self, config):
-#         super(TestModel, self).__init__(config)
-#         self.bert = BertModel(config)
-#         self.input_size = config.hidden_size
-#         self.GRU_Layer = nn.GRU(input_size=self.input_size,
-#                                 hidden_size=self.input_size//2,
-#                                 num_layers=2,
-#                                 bias=True,
-#                                 batch_first=True,
-#                                 dropout=config.hidden_dropout_prob,
-#                                 bidirectional=True)
-#         self.cls = BertOnlyMLMHead(config)
-#
-#     def forward(self, input_ids, token_type_ids, attention_mask, labels=None):
-#         outputs = self.bert(input_ids, token_type_ids, attention_mask)
-#         sequence_outputs, cls = outputs[:2]
-#         gru_outputs, _ = self.GRU_Layer(sequence_outputs)
-#         prediction_scores = self.cls(gru_outputs)
-#         masked_lm_loss = None
-#         if labels is not None:
-#             loss_fct = CrossEntropyLoss()  # -100 index = padding token
-#             masked_lm_loss = loss_fct(prediction_scores.view(-1, self.config.vocab_size), labels.view(-1))
-#
-#         output = (prediction_scores,) + outputs[2:]
-#         return ((masked_lm_loss,) + output) if masked_lm_loss is not None else output
-#
-#
-# model = TestModel.from_pretrained('bert-base-uncased')
-# tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-#
-# text = 'I love New [MASK] city.'
-# labels = tokenizer("I love New York city.", return_tensors="pt")["input_ids"]
-#
-# inputs = tokenizer(text=text, return_tensors='pt')
-# outputs = model(**inputs, labels=labels)
-#
-# print(len(outputs))
-# print(outputs[0].shape, outputs[1].shape)
-# print(outputs[0])
-# print(outputs[1])
+class TestModel(BertPreTrainedModel):
+    def __init__(self, config):
+        super(TestModel, self).__init__(config)
+        self.bert = BertModel(config)
+        self.input_size = config.hidden_size
+        self.GRU_Layer = nn.GRU(input_size=self.input_size,
+                                hidden_size=self.input_size//2,
+                                num_layers=2,
+                                bias=True,
+                                batch_first=True,
+                                dropout=config.hidden_dropout_prob,
+                                bidirectional=True)
+        self.cls = BertOnlyMLMHead(config)
+
+    def forward(self, input_ids, token_type_ids, attention_mask, labels=None):
+        outputs = self.bert(input_ids, token_type_ids, attention_mask)
+        sequence_outputs, cls = outputs[:2]
+        gru_outputs, _ = self.GRU_Layer(sequence_outputs)
+        prediction_scores = self.cls(gru_outputs)
+        masked_lm_loss = None
+        if labels is not None:
+            loss_fct = CrossEntropyLoss()  # -100 index = padding token
+            masked_lm_loss = loss_fct(prediction_scores.view(-1, self.config.vocab_size), labels.view(-1))
+
+        output = (prediction_scores,) + outputs[2:]
+        return ((masked_lm_loss,) + output) if masked_lm_loss is not None else output
+
+
+model = TestModel.from_pretrained('bert-base-uncased')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+text = 'I love New [MASK] city.'
+labels = tokenizer("I love New York city.", return_tensors="pt")["input_ids"]
+
+inputs = tokenizer(text=text, return_tensors='pt')
+outputs = model(**inputs, labels=labels)
+
+print(len(outputs))
+print(outputs[0].shape, outputs[1].shape)
+print(outputs[0])
+print(outputs[1])
 
 text1 = '今天天气怎么样？'
 text2 = '天气不太好，是雨天'
