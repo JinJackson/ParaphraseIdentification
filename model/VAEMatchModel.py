34f087f1ec759c2f1e49c3fc4350f7a13fea0405
Jackson <635725626@qq.com>
2021-11-09 00:54:23 +0800

    add mlm weight

--- model/VAEMatchModel.py
+++ model/VAEMatchModel.py
@@ -184,7 +184,7 @@ class VaeBertMatchModel(BertPreTrainedModel):
             if masked_lm_loss is None:
                 loss = loss_cvae_task
             else:
-                loss = loss_cvae_task + masked_lm_loss
+                loss = loss_cvae_task + args.mlm_weight * masked_lm_loss
             #print(task_loss, recons_loss + KLD_loss, masked_lm_loss)
             return loss, logits
         else:
