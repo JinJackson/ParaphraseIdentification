7d3d98fb9f974424db211f16e0f9a5962ffea9ab
Jackson <635725626@qq.com>
2021-11-26 11:49:41 +0800

    dataset

--- qtype_test.py
+++ qtype_test.py
@@ -20,9 +20,25 @@ data_file = 'data/LCQMC/split/what.txt'
 all_data = read_data_5lines(data_file)
 print(all_data)
 
-# class vae_Dataset(Dataset):
-#     def __init__(self, data_file, max_length, tokenizer):
-#         self.datas =
+class vae_Dataset(Dataset):
+    def __init__(self, data_file, max_length, tokenizer):
+        self.datas = read_data_5lines(data_file)
+        self.max_length = max_length
+        self.tokenizer = tokenizer
+    
+    def __getitem__(self, item):
+        data = self.datas[item]
+        query1, query2, label = data[0], data[1], int(data[2])
+        tokenzied_dict = self.tokenizer.encode_plus(text=query1,
+                                                    text_pair=query2,
+                                                    max_length=self.max_length,
+                                                    truncation=True,
+                                                    padding='max_length')
+        input_ids  = np.array(tokenzied_dict['input_ids'])
+        attention_mask = np.array(tokenzied_dict['attention_mask'])S
+        token_type_ids = np.array(tokenzied_dict['token_type_ids'])
+        return input_ids, token_type_ids, attention_mask, np.array([label]), query1, query2
+
 
 
 # def vae_Dataset():
