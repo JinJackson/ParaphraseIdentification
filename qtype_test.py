4ee8ca6e1b0cc7cc69b0cbce728710e8caeabfe4
Jackson <635725626@qq.com>
2021-11-27 10:22:50 +0800

    qtype_dataset

--- qtype_test.py
+++ qtype_test.py
@@ -1,5 +1,7 @@
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
+from utils.classification_metrics import accuracy, f1_score
+from tqdm import tqdm
 
 key_dict = {'事物': 'what', '人物': 'who', '做法': 'how', '选择': 'which', '时间': 'when', '地点': 'where', '原因': 'why',
             '其他': 'others', '未知': 'UNK'}
@@ -90,21 +92,57 @@ def load_all_datasets(model, tokenizer, test_type):
         dataset_docker = vae_Dataset
     for type in q_type:
         data_file = './data/LCQMC/split/' + key_dict[type] + '.txt'
-        all_dataset[type] = dataset_docker(data_file=data_file, max_length=max_length, tokenizer=tokenizer)
+        all_dataset[type] = dataset_docker(data_file=data_file,
+                                            max_length=max_length, 
+                                            tokenizer=tokenizer)
     return all_dataset
     
 
+def test_for_dataset(dataset, model):
+    data_loader = DataLoader(dataset=dataset, batch_size=16, shuffle=True)
+    all_labels = None
+    all_logits = None
+    loss = []
+    for batch in tqdm(data_loader, desc='testing'):
+        input_ids, token_type_ids, attention_mask, labels, query1, query2 = batch
+        outputs = model(input_ids=input_ids.long(),
+                        token_type_ids=token_type_ids.long(),
+                        attention_mask=attention_mask.long(),
+                        query1=query1,
+                        query2=query2,
+                        mask_rate=None,
+                        labels=labels)
+
+        eval_loss, logits = outputs[:2]
+
+        loss.append(eval_loss.item())
+
+    if all_labels is None:
+        all_labels = labels.detach().cpu().numpy()
+        all_logits = logits.detach().cpu().numpy()
+    else:
+        all_labels = np.concatenate((all_labels, labels.detach().cpu().numpy()), axis=0)
+        all_logits = np.concatenate((all_logits, logits.detach().cpu().numpy()), axis=0)
+
+    acc = accuracy(all_logits, all_labels)
+    f1 = f1_score(all_logits, all_labels)
+    return np.array(loss).mean(), acc, f1
+
+    
 if __name__ == "__main__":
     model = None
     from transformers import BertTokenizer
+    from model.VAEMatchModel import VaeBertMatchModel
+
     tokenzier = BertTokenizer.from_pretrained('bert-base-chinese')
-    all_dataset = load_all_datasets(model=model, tokenizer=tokenzier, test_type='vae')
+    model = VaeBertMatchModel.from_pretrained('bert-base-chinese')
+    all_dataset = load_all_datasets(model=model, 
+                                    tokenizer=tokenzier, 
+                                    test_type='vae')
     for key in all_dataset:
-        print(key) 
+        # print(key) 
         key_dataset = all_dataset[key]
-        data_loader = DataLoader(dataset=key_dataset, batch_size=2, shuffle=True)
-        for batch in data_loader:
-            input_ids, token_type_ids, attention_mask, label, query1, query2 = batch
-            print(query1, query2, input_ids)
-            break
-        break
+
+        loss, acc, f1 = test_for_dataset(dataset=key_dataset,
+                                        model=model)
+        print(key, loss, acc, f1)
