e3629f46ac1a757dd2633b6451ae6b66c209c3e3
Jackson <635725626@qq.com>
2022-03-24 09:27:33 +0800

    no kld vae model

---
+++ Train_CVAE_clean.py
@@ -0,0 +1,320 @@
+from parser1 import args
+from torch.utils.data import DataLoader
+from transformers import BertTokenizer, AdamW, get_linear_schedule_with_warmup, RobertaTokenizer, AlbertTokenizer, AutoTokenizer
+from model.MatchModel import BertMatchModel, RobertaMatchModel, AlbertMatchModel
+from model.VAEMatchModel import VaeBertMatchModel
+import os, random
+import glob
+import torch
+
+import numpy as np
+from tqdm import tqdm
+
+from all_dataset import TrainData
+from utils.logger import getLogger
+from utils.classification_metrics import accuracy, f1_score
+
+
+def seed_torch(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.enabled = False
+
+
+if args.seed > -1:
+    seed_torch(args.seed)
+
+logger = None
+
+def train(model, tokenizer, checkpoint, round):
+    if args.fp16:
+        try:
+            from apex import amp
+        except ImportError:
+            raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use fp16 training.")
+    else:
+        amp = None
+
+    train_data = TrainData(data_file=args.train_file,
+                           max_length=args.max_length,
+                           tokenizer=tokenizer,
+                           model_type=args.model_type)
+
+    train_dataloader = DataLoader(dataset=train_data,
+                                  batch_size=args.batch_size,
+                                  shuffle=True)
+
+    t_total = len(train_dataloader) * args.epochs
+    warmup_steps = int(args.warmup_steps * t_total)
+    optimizer = AdamW(model.parameters(), lr=args.learning_rate, eps=args.adam_epsilon)
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=warmup_steps, num_training_steps=t_total)
+
+    if args.fp16:
+        model, optimizer = amp.initialize(model, optimizer, opt_level=args.fptype)
+
+    # 读取断点 optimizer、scheduler
+    checkpoint_dir = args.save_dir + "/checkpoint-" + str(checkpoint) + '-' + str(round)
+    if os.path.isfile(os.path.join(checkpoint_dir, "optimizer.pt")):
+        # Load in optimizer and scheduler states
+        optimizer.load_state_dict(torch.load(os.path.join(checkpoint_dir, "optimizer.pt")))
+        scheduler.load_state_dict(torch.load(os.path.join(checkpoint_dir, "scheduler.pt")))
+        if args.fp16:
+            amp.load_state_dict(torch.load(os.path.join(checkpoint_dir, "amp.pt")))
+
+    # 开始训练
+    logger.info("***** Running training *****")
+    logger.info("  Num examples = %d", len(train_dataloader))
+    logger.info("  Num Epochs = %d", args.epochs)
+    logger.info("  Batch size = %d", args.batch_size)
+    logger.info("  learning_rate = %s", str(args.learning_rate))
+    logger.info("  Total steps = %d", t_total)
+    logger.info("  warmup steps = %d", warmup_steps)
+    logger.info("  Model_type = %s", args.model_type)
+    logger.info("  Decoder_type = %s", args.decoder_type)
+    logger.info("  MLM task = %s", str(args.mlm))
+    logger.info("  MLM weight = %s", str(args.mlm_weight))
+
+
+    # 没有历史断点，则从0开始
+    if checkpoint < 0:
+        checkpoint = 0
+        round = 0
+    else:
+        checkpoint += 1
+        round += 1
+
+    max_test_acc = 0
+    max_test_f1 = 0
+
+    max_dev_acc = 0
+    max_dev_f1 = 0
+
+    logger.debug("  Start Batch = %d", checkpoint)
+    for epoch in range(checkpoint, args.epochs):
+        model.train()
+        epoch_loss = []
+
+        step = 0
+        for batch in tqdm(train_dataloader, desc="Iteration", ncols=50):
+            model.zero_grad()
+            # 设置tensor gpu运行
+            query1, query2 = batch[-2:]
+            batch = tuple(t.to(args.device) for t in batch[:-2])
+
+            if 'roberta' in args.model_type:
+                input_ids, attention_mask, labels = batch
+                outputs = model(input_ids=input_ids.long(),
+                                attention_mask=attention_mask.long(),
+                                labels=labels)
+
+            else:
+                input_ids, token_type_ids, attention_mask, labels = batch
+                outputs = model(input_ids=input_ids.long(),
+                                token_type_ids=token_type_ids.long(),
+                                attention_mask=attention_mask.long(),
+                                query1=query1,
+                                query2=query2,
+                                mask_rate=args.mask_rate,
+                                labels=labels)
+
+            loss = outputs[0]
+
+            if args.fp16:
+                with amp.scale_loss(loss, optimizer) as scaled_loss:
+                    scaled_loss.backward()
+            else:
+                loss.backward()  # 计算出梯度
+
+            epoch_loss.append(loss.item())
+
+            optimizer.step()
+            scheduler.step()
+            step += 1
+            if step % 500 == 0:
+                logger.debug("loss:"+str(np.array(epoch_loss).mean()))
+                logger.debug('learning_rate:' + str(optimizer.state_dict()['param_groups'][0]['lr']))
+            if step % args.saving_steps == 0:
+                round += 1
+                dev_loss, dev_acc, dev_f1 = test(model=model, tokenizer=tokenizer, test_file=args.dev_file,
+                                                 checkpoint=epoch, round=round)
+                logger.info(
+                    '【DEV】Train Epoch %d, round %d: train_loss=%.4f, acc=%.4f, f1=%.4f' % (
+                    epoch, round, dev_loss, dev_acc, dev_f1))
+
+                test_loss, test_acc, test_f1 = test(model=model, tokenizer=tokenizer, test_file=args.test_file,
+                                                    checkpoint=epoch, round=round)
+                logger.info(
+                    '【TEST】Train Epoch %d, round %d: train_loss=%.4f, acc=%.4f, f1=%.4f' % (
+                    epoch, round, test_loss, test_acc, test_f1))
+                output_dir = args.save_dir + "/checkpoint-" + str(epoch) + '-' + str(round)
+                if test_acc >= max_test_acc or test_f1 >= max_test_f1 or dev_acc >= max_dev_acc or dev_f1 >= max_dev_f1:
+                    max_test_acc = max(test_acc, max_test_acc)
+                    max_test_f1 = max(test_f1, max_test_f1)
+                    max_dev_acc = max(dev_acc, max_dev_acc)
+                    max_dev_f1 = max(dev_f1, max_dev_f1)
+
+                    if not os.path.exists(output_dir):
+                        os.makedirs(output_dir)
+                    model_to_save = (model.module if hasattr(model, "module") else model)
+                    model_to_save.save_pretrained(output_dir)
+                    tokenizer.save_pretrained(output_dir)
+                    torch.save(args, os.path.join(output_dir, "training_args.bin"))
+                    logger.debug("Saving model checkpoint to %s", output_dir)
+                    if args.fp16:
+                        torch.save(amp.state_dict(), os.path.join(output_dir, "amp.pt"))
+                    torch.save(optimizer.state_dict(), os.path.join(output_dir, "optimizer.pt"))
+                    torch.save(scheduler.state_dict(), os.path.join(output_dir, "scheduler.pt"))
+                    logger.debug("Saving optimizer and scheduler states to %s", output_dir)
+                model.train()
+
+            # 保存模型
+        output_dir = args.save_dir + "/checkpoint-" + str(epoch) + '-' + str(round)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        model_to_save = (model.module if hasattr(model, "module") else model)
+        model_to_save.save_pretrained(output_dir)
+        tokenizer.save_pretrained(output_dir)
+        torch.save(args, os.path.join(output_dir, "training_args.bin"))
+        logger.debug("Saving model checkpoint to %s", output_dir)
+        if args.fp16:
+            torch.save(amp.state_dict(), os.path.join(output_dir, "amp.pt"))
+        torch.save(optimizer.state_dict(), os.path.join(output_dir, "optimizer.pt"))
+        torch.save(scheduler.state_dict(), os.path.join(output_dir, "scheduler.pt"))
+        logger.debug("Saving optimizer and scheduler states to %s", output_dir)
+
+        dev_loss, dev_acc, dev_f1 = test(model=model, tokenizer=tokenizer, test_file=args.dev_file, checkpoint=epoch, round=round)
+        test_loss, test_acc, test_f1 = test(model=model, tokenizer=tokenizer, test_file=args.test_file, checkpoint=epoch, round=round)
+        #print(test_loss, test_acc)
+        logger.info(
+            '【DEV】Train Epoch %d, round %d: train_loss=%.4f, acc=%.4f, f1=%.4f' % (
+            epoch, round, dev_loss, dev_acc, dev_f1))
+        logger.info(
+            '【TEST】Train Epoch %d, round %d: train_loss=%.4f, acc=%.4f, f1=%.4f' % (
+            epoch, round, test_loss, test_acc, test_f1))
+        if test_acc >= max_test_acc or test_f1 >= max_test_f1 or dev_acc >= max_dev_acc or dev_f1 >= max_dev_f1:
+            max_test_acc = max(test_acc, max_test_acc)
+            max_test_f1 = max(test_f1, max_test_f1)
+            max_dev_acc = max(dev_acc, max_dev_acc)
+            max_dev_f1 = max(dev_f1, max_dev_f1)
+    logger.info('【BEST TEST ACC】: %.4f,   【BEST TEST F1】: %.4f' % (max_test_acc, max_test_f1))
+    logger.info('【BEST DEV ACC】: %.4f,   【BEST DEV F1】: %.4f' % (max_dev_acc, max_dev_f1))
+
+def test(model, tokenizer, test_file, checkpoint, round, output_dir=None):
+    print(type(model))
+    test_data = TrainData(data_file=test_file,
+                          max_length=args.max_length,
+                          tokenizer=tokenizer,
+                          model_type=args.model_type)
+
+    test_dataLoader = DataLoader(dataset=test_data,
+                                 batch_size=args.batch_size,
+                                 shuffle=False)
+
+    logger.debug("***** Running test {} for round {}*****".format(checkpoint, round))
+    logger.debug("  Num examples = %d", len(test_dataLoader))
+    logger.debug("  Batch size = %d", args.batch_size)
+
+    loss = []
+
+    all_labels = None
+    all_logits = None
+
+    model.eval()
+
+    for batch in tqdm(test_dataLoader, desc="Evaluating", ncols=50):
+        with torch.no_grad():
+            query1, query2 = batch[-2:]
+            batch = [t.to(args.device) for t in batch[:-2]]
+            if 'roberta' in args.model_type:
+                input_ids, attention_mask, labels = batch
+                outputs = model(input_ids=input_ids.long(),
+                                attention_mask=attention_mask.long(),
+                                labels=labels)
+
+            else:
+                input_ids, token_type_ids, attention_mask, labels = batch
+                outputs = model(input_ids=input_ids.long(),
+                                token_type_ids=token_type_ids.long(),
+                                attention_mask=attention_mask.long(),
+                                query1=query1,
+                                query2=query2,
+                                mask_rate=None,
+                                labels=labels)
+
+            eval_loss, logits = outputs[:2]
+
+            loss.append(eval_loss.item())
+
+            if all_labels is None:
+                all_labels = labels.detach().cpu().numpy()
+                all_logits = logits.detach().cpu().numpy()
+            else:
+                all_labels = np.concatenate((all_labels, labels.detach().cpu().numpy()), axis=0)
+                all_logits = np.concatenate((all_logits, logits.detach().cpu().numpy()), axis=0)
+
+    acc = accuracy(all_logits, all_labels)
+    f1 = f1_score(all_logits, all_labels)
+    return np.array(loss).mean(), acc, f1
+
+
+if __name__ == "__main__":
+
+    # 创建存储目录
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    logger = getLogger(__name__, os.path.join(args.save_dir, 'log.txt'))
+
+    if 'roberta' in args.model_type:
+        MatchModel = RobertaMatchModel
+        Tokenizer = RobertaTokenizer
+    elif 'albert' in args.model_type:
+        MatchModel = AlbertMatchModel
+        Tokenizer = AlbertTokenizer
+    elif 'bert' in args.model_type:
+        MatchModel = VaeBertMatchModel
+        Tokenizer = BertTokenizer
+    elif 'ernie' in args.model_type:
+        MatchModel = VaeBertMatchModel
+        Tokenizer = AutoTokenizer
+
+    if args.do_train:
+        # train： 接着未训练完checkpoint继续训练
+        checkpoint = -1
+        round = -1
+        for checkpoint_dir_name in glob.glob(args.save_dir + "/*"):
+            try:
+                checkpoint = max(checkpoint, int(checkpoint_dir_name.split('/')[-1].split('-')[1]))
+                round = max(round, int(checkpoint_dir_name.split('/')[-1].split('-')[-1]))
+            except Exception as e:
+                pass
+        checkpoint_dir = args.save_dir + "/checkpoint-" + str(checkpoint) + '-' + str(round)
+        if checkpoint > -1:
+            logger.debug(f" Load Model from {checkpoint_dir}")
+
+        tokenizer = Tokenizer.from_pretrained(args.model_type if checkpoint == -1 else checkpoint_dir,
+                                              do_lower_case=args.do_lower_case)
+        model = MatchModel.from_pretrained(args.model_type if checkpoint == -1 else checkpoint_dir)
+        model.to(args.device)
+        # 训练
+        train(model, tokenizer, checkpoint, round)
+
+    else:
+        # eval：指定模型
+        checkpoint = args.checkpoint
+        round = args.round
+        checkpoint_dir = args.save_dir + "/checkpoint-" + str(checkpoint) + '-' + str(round)
+        tokenizer = Tokenizer.from_pretrained(checkpoint_dir,
+                                              do_lower_case=args.do_lower_case)
+        model = MatchModel.from_pretrained(checkpoint_dir)
+        model.to(args.device)
+        # 评估
+        test_loss, test_acc, test_f1 = test(model, tokenizer, test_file=args.test_file, checkpoint=checkpoint, round=round)
+        logger.debug('Evaluate Epoch %d: test_loss=%.4f, test_acc=%.4f, test_f1=%.4f' % (
+        checkpoint, test_loss, test_acc, test_f1))
--- model/VAEMatchModel.py
+++ model/VAEMatchModel.py
@@ -191,7 +191,88 @@ class VaeBertMatchModel(BertPreTrainedModel):
         else:
             return logits
 
+# VAE MatchModel
+class VaeBertMatchModelClean(BertPreTrainedModel):
+    def __init__(self, config):
+        super(VaeBertMatchModel, self).__init__(config)
+        self.bert = BertModel(config)
+        # cvae返回(latent_z, output) output就是重构的x:[batch,seq,768]
+        # lantent_z = [batch, seq*hidden]
+        self.input_size = config.hidden_size
+        self.dropout = config.hidden_dropout_prob
+        self.num_layers = args.num_layers
+        self.decoder_type = args.decoder_type
+        self.vae_module = VaeModel(input_size=self.input_size,
+                                   num_layers=self.num_layers,
+                                   dropout=self.dropout,
+                                   decoder_type=self.decoder_type)
+        self.cls = BertOnlyMLMHead(config)
+        # 加一个FFN
+        # self.linear1 = nn.Linear(seq_len*hidden_size, seq_len*hidden_size*2)
+        # self.linear2 = nn.Linear(seq_len*hidden_size*2, seq_len*hidden_size)
+        self.linear3 = nn.Linear(self.input_size, 1)
+        self.reconstruction_loss_func = nn.MSELoss()
+        self.task_loss_func = nn.BCEWithLogitsLoss()
 
+    def forward(self, input_ids, token_type_ids, attention_mask, query1, query2, mask_rate=None, labels=None):
+
+        outputs = self.bert(input_ids=input_ids,
+                            token_type_ids=token_type_ids,
+                            attention_mask=attention_mask)
+
+        last_hidden_state = outputs[0]
+
+        mean, logvar, latent_z, recons_x, encoder_outputs = self.vae_module(representation=last_hidden_state)
+        cls = encoder_outputs[:, 0, :]
+        logits = self.linear3(cls)
+
+        if labels is not None:
+            # MLM任务
+            masked_lm_loss = None
+            if args.mlm and (mask_rate is not None):
+                mask_nums = [int(mask_rate * (len(q1) + len(q2))) for q1, q2 in zip(query1, query2)]
+                # 从答案复制一份出来mask
+                masked_input_ids = input_ids.clone().cpu().detach().numpy()
+                # 把结果送进去算attention，决定mask什么
+                attn_outputs, p_attn = attention(query=encoder_outputs,
+                                                 key=encoder_outputs,
+                                                 value=encoder_outputs,
+                                                 mask=attention_mask)
+                # 每个词共获得的注意力大小
+                attn_word = torch.sum(p_attn, dim=1)
+                # 按照注意力大小对单词排序
+                sort_idx = torch.argsort(attn_word, dim=1, descending=True).cpu().detach().numpy()
+                # 按照每句话不同的数量进行mask
+                for mask_num, elem, sort_idx in zip(mask_nums, masked_input_ids, sort_idx):
+                    for i in range(mask_num):
+                        mask_idx = sort_idx[i]
+                        elem[mask_idx] = 103
+                masked_input_ids = torch.tensor(masked_input_ids).to(args.device)
+                # 将mask传入模型计算结果·
+                masked_outputs = self.bert(input_ids=masked_input_ids,
+                                           token_type_ids=token_type_ids,
+                                           attention_mask=attention_mask)
+
+                masked_sequence_output, masked_cls = masked_outputs[:2]
+                prediction_scores = self.cls(masked_sequence_output)
+                masked_lm_labels = input_ids
+                loss_fct = CrossEntropyLoss()  # -100 index = padding token
+                masked_lm_loss = loss_fct(prediction_scores.view(-1, self.config.vocab_size), masked_lm_labels.view(-1))
+                # print(masked_lm_loss)
+            task_loss = self.task_loss_func(logits, labels.float())
+            recons_loss = self.reconstruction_loss_func(recons_x, last_hidden_state)
+            #KLD_loss = -0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp())
+            #loss_cvae_task = args.task_weight * task_loss + (1 - args.task_weight) * (recons_loss + KLD_loss)
+            loss_cvae_task = args.task_weight * task_loss + (1 - args.task_weight) * (recons_loss + KLD_loss)
+
+            if masked_lm_loss is None:
+                loss = loss_cvae_task
+            else:
+                loss = loss_cvae_task + args.mlm_weight * masked_lm_loss
+            #print(task_loss, recons_loss + KLD_loss, masked_lm_loss)
+            return loss, logits
+        else:
+            return logits
 
 # VAE + Multi-task MatchModel
 class VaeMultiTaskMatchModel(BertPreTrainedModel):
